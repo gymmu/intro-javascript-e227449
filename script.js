9ce34965aa89802f5c6167afd69cc3f41c2a1490
Luisa Rudin <e227449@edu.sbl.ch>
2023-10-17 09:31:10 +0200

    Ende Lektion

--- script.js
+++ script.js
@@ -9,6 +9,46 @@ function changeBackground() {
 
 
 function deleteText () {
- id="#to-delete "  
+ 
+ const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"  
+    }
+
+    function incrementCounter() {
+      const numberElement = document.querySelector("#counter")
+      let num = parseInt(numberElement.textContent)
+      num = num + 1
+      numberElement.textContent = num
+    }
+
+
+    function decrementCounter() {
+      const numberElement = document.querySelector("#counter")
+      let num = parseInt(numberElement.textContent)
+      num = num - 1
+      numberElement.textContent = num
     
-}
+    }
+
+    function resetCounter () {
+      const numberElement = document.querySelector("#counter")
+      let num = parseInt(numberElement.textContent)
+      num = 0
+      numberElement.textContent = num
+    
+    }
+
+
+
+
+    function writeText(){
+      const inputElem = document.querySelector("#input")
+      const outputElem = document.querySelector("#output")
+      
+      const inputValue = inputElem.value
+      const outputValue = outputElem.textContent
+
+      const newValue = outputValue + "\n" + inputValue
+
+      outputElem.textContent = new Value
+  }
